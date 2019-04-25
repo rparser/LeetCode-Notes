7673f787a462db4c35e1c9f35e206b20ea8dbefd
rparser <runningparser@gmail.com>
2019-04-25 04:57:15 -0500

    236

--- src/com/leetcode/solution/_235LowestCommonAncestorofaBinarySearchTree.java
+++ src/com/leetcode/solution/_235LowestCommonAncestorofaBinarySearchTree.java
@@ -2,7 +2,9 @@ package com.leetcode.solution;
 
 /**
  * 最近公共祖先BST
- *
+ * <p>
+ * Time Complexity: O(N), where NN is the number of nodes in the BST. In the worst case we might be visiting all the nodes of the BST.
+ * Space Complexity: O(N). This is because the maximum amount of space utilized by the recursion stack would be N since the height of a skewed BST could be N.
  */
 
 public class _235LowestCommonAncestorofaBinarySearchTree {
