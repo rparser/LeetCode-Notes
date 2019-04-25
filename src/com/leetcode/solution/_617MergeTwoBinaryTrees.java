16a7bf768982c735defd2823fe6b9ae37889b347
rparser <runningparser@gmail.com>
2019-04-25 04:52:39 -0500

    617

---
+++ src/com/leetcode/solution/_617MergeTwoBinaryTrees.java
@@ -0,0 +1,58 @@
+package com.leetcode.solution;
+
+import java.util.Stack;
+
+/**
+ * 合并BFS
+ * <p>
+ * Time complexity : O(m). A total of mm nodes need to be traversed. Here, m represents the minimum number of nodes from the two given trees.
+ * Space complexity : O(m). The depth of the recursion tree can go upto m in the case of a skewed tree. In average case, depth will be O(logm).
+ */
+
+public class _617MergeTwoBinaryTrees {
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if (t1 == null)
+            return t2;
+        if (t2 == null)
+            return t1;
+        t1.val += t2.val;
+        t1.left = mergeTrees(t1.left, t2.left);
+        t1.right = mergeTrees(t1.right, t2.right);
+        return t1;
+    }
+
+    public TreeNode mergeTreesIterative(TreeNode t1, TreeNode t2) {
+        if (t1 == null)
+            return t2;
+        Stack<TreeNode[]> stack = new Stack<>();
+        stack.push(new TreeNode[]{t1, t2});
+        while (!stack.isEmpty()) {
+            TreeNode[] t = stack.pop();
+            if (t[0] == null || t[1] == null) {
+                continue;
+            }
+            t[0].val += t[1].val;
+            if (t[0].left == null) {
+                t[0].left = t[1].left;
+            } else {
+                stack.push(new TreeNode[]{t[0].left, t[1].left});
+            }
+            if (t[0].right == null) {
+                t[0].right = t[1].right;
+            } else {
+                stack.push(new TreeNode[]{t[0].right, t[1].right});
+            }
+        }
+        return t1;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
