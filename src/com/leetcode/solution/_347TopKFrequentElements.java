620fd054e8e146df5141b32207e47ea0136faa86
rparser <runningparser@gmail.com>
2019-04-26 02:54:06 -0500

    347

---
+++ src/com/leetcode/solution/_347TopKFrequentElements.java
@@ -0,0 +1,61 @@
+package com.leetcode.solution;
+
+import java.util.*;
+
+
+public class _347TopKFrequentElements {
+    public List<Integer> topKFrequent(int[] nums, int k) {
+        // build hash map : character and how often it appears
+        HashMap<Integer, Integer> count = new HashMap<>();
+        for (int n : nums) {
+            count.put(n, count.getOrDefault(n, 0) + 1);
+        }
+
+        // init heap 'the less frequent element first'
+//        PriorityQueue<Integer> heap =
+//                new PriorityQueue<>((n1, n2) -> count.get(n1) - count.get(n2));
+        PriorityQueue<Integer> heap =
+                new PriorityQueue<>(Comparator.comparingInt(count::get));
+
+        // keep k top frequent elements in the heap
+        for (int n : count.keySet()) {
+            heap.add(n);
+            if (heap.size() > k)
+                heap.poll();
+        }
+
+        // build output list
+        List<Integer> top_k = new LinkedList<>();
+        while (!heap.isEmpty())
+            top_k.add(heap.poll());
+        Collections.reverse(top_k);
+        return top_k;
+    }
+
+    public List<Integer> topKFrequentON(int[] nums, int k) {
+
+        List<Integer>[] bucket = new List[nums.length + 1];
+        Map<Integer, Integer> frequencyMap = new HashMap<Integer, Integer>();
+
+        for (int n : nums) {
+            frequencyMap.put(n, frequencyMap.getOrDefault(n, 0) + 1);
+        }
+
+        for (int key : frequencyMap.keySet()) {
+            int frequency = frequencyMap.get(key);
+            if (bucket[frequency] == null) {
+                bucket[frequency] = new ArrayList<>();
+            }
+            bucket[frequency].add(key);
+        }
+
+        List<Integer> res = new ArrayList<>();
+
+        for (int pos = bucket.length - 1; pos >= 0 && res.size() < k; pos--) {
+            if (bucket[pos] != null) {
+                res.addAll(bucket[pos]);
+            }
+        }
+        return res;
+    }
+}
