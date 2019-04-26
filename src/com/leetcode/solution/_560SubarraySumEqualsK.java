b356be193961238d2ed3c863fe71a6bf10f542e6
rparser <runningparser@gmail.com>
2019-04-26 01:54:15 -0500

    560

---
+++ src/com/leetcode/solution/_560SubarraySumEqualsK.java
@@ -0,0 +1,25 @@
+package com.leetcode.solution;
+
+import java.util.*;
+
+/**
+ * 子数组和为K
+ *
+ * Time complexity : O(n). The entire nums array is traversed only once.
+ * Space complexity : O(n). Hashmap can contain upto nn distinct entries in the worst case.
+ */
+
+public class _560SubarraySumEqualsK {
+    public int subarraySum(int[] nums, int k) {
+        int result = 0, sum = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (map.containsKey(sum - k))
+                result += map.get(sum - k);
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+        return result;
+    }
+}
