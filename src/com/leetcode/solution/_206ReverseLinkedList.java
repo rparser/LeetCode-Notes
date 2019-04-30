3301aca748522bd850cfbc3d89dea15f1af59a19
rparser <runningparser@gmail.com>
2019-04-30 03:43:27 -0500

    Update _206ReverseLinkedList.java

--- src/com/leetcode/solution/_206ReverseLinkedList.java
+++ src/com/leetcode/solution/_206ReverseLinkedList.java
@@ -5,13 +5,15 @@ import org.junit.runner.*;
 
 public class _206ReverseLinkedList {
     public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) return head;
         ListNode prev = null;
         ListNode curr = head;
+        ListNode next = curr.next; // 下一个node存储在临时位置
         while (curr != null) {
-            ListNode temp = curr.next; // 下一个node存储在临时位置
             curr.next = prev; // 当前next指向前一个node
             prev = curr;  // 当前curr已经计算完毕 赋值前一个prev
-            curr = temp;  // 保存的temp赋值给当前curr计算下一个
+            curr = next;  // 保存的temp赋值给当前curr计算下一个
+            if (curr != null) next = curr.next;
         }
         return prev;
     }
