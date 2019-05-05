0d84c8dfcc76dbe86aa1e3b81d6fa3bf61a91409
rparser <runningparser@gmail.com>
2019-05-04 19:37:51 -0500

    102

---
+++ src/com/leetcode/solution/_102BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,41 @@
+package com.leetcode.solution;
+
+import java.util.*;
+
+/**
+ * 树分层遍历
+ *
+ * Time complexity : O(N) since each node is processed exactly once.
+ * Space complexity : O(N) to keep the output structure which contains N node values.
+ */
+
+public class _102BinaryTreeLevelOrderTraversal {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) return result;
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size(); //设定当前层有几个节点
+            LinkedList<Integer> currLevel = new LinkedList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.pop();
+                currLevel.add(node.val); //把queue里小于size的结果加入当前层list
+                if (node.left != null) queue.offer(node.left);
+                if (node.right != null) queue.offer(node.right);
+            }
+            result.add(currLevel); //当前层list加入result
+        }
+        return result;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
