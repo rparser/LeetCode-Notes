e5159941af2400d58734b73a135eca520c1f097c
rparser <runningparser@gmail.com>
2019-05-04 20:30:27 -0500

    200

--- src/com/leetcode/solution/_200NumberofIslands.java
+++ src/com/leetcode/solution/_200NumberofIslands.java
@@ -1,12 +1,16 @@
 package com.leetcode.solution;
 
 /**
+ * 岛屿数目
+ * <p>
  * DFS 解法：把每个为1的点的相邻点全变成0
+ * Time complexity : O(M×N) where MM is the number of rows and N is the number of columns.
+ * Space complexity : worst case O(M×N) in case that the grid map is filled with lands where DFS goes by M×N deep.
  */
 
 public class _200NumberofIslands {
     void dfs(char[][] grid, int r, int c) {
-        if (r < 0 || c < 0 || r >= grid.length || c >= grid[0].length || grid[r][c] == '0') { //超出范围或该点为0
+        if (r < 0 || c < 0 || r >= grid.length || c >= grid[0].length || grid[r][c] == '0') { //该点为0或超出范围，立刻中断返回
             return;
         }
         grid[r][c] = '0'; //把该点变成0
@@ -24,7 +28,7 @@ public class _200NumberofIslands {
         for (int r = 0; r < grid.length; ++r) {
             for (int c = 0; c < grid[0].length; ++c) { //遍历每个点
                 if (grid[r][c] == '1') { //如果该点为陆地
-                    num_islands++; //岛屿数为0
+                    num_islands++; //岛屿数+1
                     dfs(grid, r, c); //dfs搜索周围的点
                 }
             }
