7ce2bae6e107072ccad1f2737a8b1c30280e96e6
rparser <runningparser@gmail.com>
2019-05-26 06:03:45 -0500

    Update _035SearchInsertPosition.java

--- src/com/leetcode/solution/_035SearchInsertPosition.java
+++ src/com/leetcode/solution/_035SearchInsertPosition.java
@@ -1,7 +1,7 @@
 package com.leetcode.solution;
 
 /**
- * 二分法查找
+ * 二分法查找,参考033
  * <p>
  * low,high两坐标，中值大于target则high=mid-1，中值小于target则low=mid+1
  */
@@ -10,7 +10,7 @@ public class _035SearchInsertPosition {
     public int searchInsert(int[] nums, int target) {
         int low = 0, high = nums.length - 1; //数组最大值
         while (low <= high) {
-            int mid = (low + high) / 2; //10数组(0-9)返回mid 4(中值偏下)，9数组返回mid 4(中值)
+            int mid = low + (high - low) / 2;
             if (nums[mid] == target) return mid;
             if (nums[mid] > target) high = mid - 1;
             else low = mid + 1;
