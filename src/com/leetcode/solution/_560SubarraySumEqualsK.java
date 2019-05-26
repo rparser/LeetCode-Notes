ee1dc7e76677526d1144e8e526a7791db46a9719
rparser <runningparser@gmail.com>
2019-05-26 04:44:50 -0500

    Update _560SubarraySumEqualsK.java

--- src/com/leetcode/solution/_560SubarraySumEqualsK.java
+++ src/com/leetcode/solution/_560SubarraySumEqualsK.java
@@ -7,16 +7,17 @@ import java.util.*;
  * <p>
  * Time complexity : O(n). The entire nums array is traversed only once.
  * Space complexity : O(n). Hashmap can contain upto nn distinct entries in the worst case.
+ * 1loop，建map<当前sum，能达到这个sum的可能数>,把每个sum加入map，用containsKey(sum-k)得到result
  */
 
 public class _560SubarraySumEqualsK {
     public int subarraySum(int[] nums, int k) {
         int result = 0, sum = 0;
-        HashMap<Integer, Integer> map = new HashMap<>();
+        HashMap<Integer, Integer> map = new HashMap<>(); //<当前sum，能达到这个sum的可能数>
         map.put(0, 1);
         for (int num : nums) {
             sum += num;
-            if (map.containsKey(sum - k))
+            if (map.containsKey(sum - k)) //a1a2a3a4a5,若a3a4为K，则sum计算到a4时，a1a2的sum必在map里
                 result += map.get(sum - k);
             map.put(sum, map.getOrDefault(sum, 0) + 1);
         }
