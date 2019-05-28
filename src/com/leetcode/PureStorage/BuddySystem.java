76f5a984c8cf4a67368f664c0b2c7d19da76ae36
rparser <runningparser@gmail.com>
2019-05-28 03:44:10 -0500

    Create BuddySystem.java

---
+++ src/com/leetcode/PureStorage/BuddySystem.java
@@ -0,0 +1,40 @@
+package com.leetcode.PureStorage;
+
+public class BuddySystem {
+    public static void clearBit(int[][] matrix,  int offset, int length) {
+        int curLevel = matrix.length-1;
+        int left = offset;
+        int right = offset+length - 1;
+        while (curLevel >= 0){
+            for (int i = left; i <= right; i++) {
+                matrix[curLevel][i] = 0;
+            }
+            curLevel--;
+            left = left/2;
+            right = right/2;
+        }
+    }
+    public static void setBit(int[][] matrix,  int offset, int length) {
+        int curLevel = matrix.length-1;
+        int left = offset;
+        int right = offset+length - 1;
+        while (curLevel >= 0) {
+            for (int i = left; i <= right; i++) {
+                matrix[curLevel][i] = 1;
+            }
+            int leftBuddy = left + (left % 2 == 1 ? -1 : 1);
+            int rightBuddy = right + (right % 2 == 1 ? -1 : 1);
+            int leftBit = matrix[curLevel][left] * matrix[curLevel][leftBuddy];
+            int rightBit = matrix[curLevel][right] * matrix[curLevel][rightBuddy];
+            curLevel--;
+            left /= 2;
+            right /= 2;
+            if (leftBit == 0) {
+                left++;
+            }
+            if (rightBit == 0) {
+                right--;
+            }
+        }
+    }
+}
