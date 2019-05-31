998381423da7c7ecd216c7d2c2e3735ab40d80d4
rparser <runningparser@gmail.com>
2019-05-30 22:10:14 -0500

    Update _314_BinaryTreeVerticalOrderTraversal.java

--- src/com/leetcode/solution/_314_BinaryTreeVerticalOrderTraversal.java
+++ src/com/leetcode/solution/_314_BinaryTreeVerticalOrderTraversal.java
@@ -9,36 +9,33 @@ import java.util.*;
  * Complexity: O(N) time O(N) space
  * <p>
  * 关键字：BFS, two queue
+ * 2队列(node和距离),map距离:list<path>，int min/max距离。模板while，取点，如果map没有则建新list,添加左右子树进队列
  */
 
 public class _314_BinaryTreeVerticalOrderTraversal {
     public List<List<Integer>> verticalOrder(TreeNode root) {
         List<List<Integer>> result = new ArrayList<>();
         if (root == null) return result;
-        Queue<TreeNode> nodes = new LinkedList<>();
-        Queue<Integer> dist = new LinkedList<>();
+        Queue<TreeNode> nodeQueue = new LinkedList<>();
+        Queue<Integer> distQueue = new LinkedList<>();
         HashMap<Integer, List<Integer>> map = new HashMap<>();//<dist, path>
-        nodes.add(root);
-        dist.add(0);
-        int min = 0, max = 0;
-        while (!nodes.isEmpty()) {
-            TreeNode cur = nodes.poll();
-            int curdis = dist.poll();
-            if (!map.containsKey(curdis)) map.put(curdis, new ArrayList<>());
-            map.get(curdis).add(cur.val);
-            if (cur.left != null) {
-                nodes.add(cur.left);
-                dist.add(curdis - 1);
-                if (min > curdis - 1) {
-                    min = curdis - 1;
-                }
+        nodeQueue.add(root);
+        distQueue.add(0);
+        int min = 0, max = 0; //记录最小最大位置
+        while (!nodeQueue.isEmpty()) {
+            TreeNode cur = nodeQueue.poll();
+            int dist = distQueue.poll();
+            if (!map.containsKey(dist)) map.put(dist, new ArrayList<>());
+            map.get(dist).add(cur.val);
+            if (cur.left != null) { //左子树加入队列
+                nodeQueue.add(cur.left);
+                distQueue.add(dist - 1); //位置-1
+                if (min > dist - 1) min = dist - 1;
             }
-            if (cur.right != null) {
-                nodes.add(cur.right);
-                dist.add(curdis + 1);
-                if (max < curdis + 1) {
-                    max = curdis + 1;
-                }
+            if (cur.right != null) { //右子树加入队列
+                nodeQueue.add(cur.right);
+                distQueue.add(dist + 1); //位置+1
+                if (max < dist + 1) max = dist + 1;
             }
         }
         for (int i = min; i <= max; i++) {
