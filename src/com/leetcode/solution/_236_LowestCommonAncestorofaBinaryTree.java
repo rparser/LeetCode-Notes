26c19d2af1181dd737daf7373c1dbd2d28c92cb1
rparser <runningparser@gmail.com>
2019-05-31 05:01:40 -0500

    Update _236_LowestCommonAncestorofaBinaryTree.java

--- src/com/leetcode/solution/_236_LowestCommonAncestorofaBinaryTree.java
+++ src/com/leetcode/solution/_236_LowestCommonAncestorofaBinaryTree.java
@@ -1,50 +1,51 @@
 package com.leetcode.solution;
 
+import java.util.*;
+
 /**
  * 最近公共祖先普通
  * <p>
- * Time Complexity: O(N), where NN is the number of nodes in the binary tree. In the worst case we might be visiting all the nodes of the binary tree.
- * Space Complexity: O(N). This is because the maximum amount of space utilized by the recursion stack would be N since the height of a skewed binary tree could be N.
+ * Time Complexity : O(N), where NN is the number of nodes in the binary tree.
+ * In the worst case we might be visiting all the nodes of the binary tree.
+ * <p>
+ * Space Complexity : O(N). In the worst case space utilized by the stack, the parent pointer dictionary and the ancestor set,
+ * would be N each, since the height of a skewed binary tree could be NN.
+ * <p>
+ * 建queue BFS,map(node : parent of node)，while到pq都进入map,建set while存p所有祖先，while q祖先找第一出现的
  */
 
 public class _236_LowestCommonAncestorofaBinaryTree {
-
-    private TreeNode ans;
-
-    public _236_LowestCommonAncestorofaBinaryTree() {
-        // Variable to store LCA node.
-        this.ans = null;
-    }
-
-    private boolean recurseTree(TreeNode currentNode, TreeNode p, TreeNode q) {
-
-        // If reached the end of a branch, return false.
-        if (currentNode == null) {
-            return false;
-        }
-
-        // Left Recursion. If left recursion returns true, set left = 1 else 0
-        int left = this.recurseTree(currentNode.left, p, q) ? 1 : 0;
-
-        // Right Recursion
-        int right = this.recurseTree(currentNode.right, p, q) ? 1 : 0;
-
-        // If the current node is one of p or q
-        int mid = (currentNode == p || currentNode == q) ? 1 : 0;
-
-        // If any two of the flags left, right or mid become True
-        if (mid + left + right >= 2) {
-            this.ans = currentNode;
-        }
-
-        // Return true if any one of the three bool values is True.
-        return (mid + left + right > 0);
-    }
-
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        // Traverse the tree
-        this.recurseTree(root, p, q);
-        return this.ans;
+        Queue<TreeNode> stack = new LinkedList<>(); //队列存结果
+        Map<TreeNode, TreeNode> parent = new HashMap<>(); //key : value - node, node的父节点
+
+        parent.put(root, null);
+        stack.offer(root);
+
+        while (!parent.containsKey(p) || !parent.containsKey(q)) { //直到p,q的都压入
+
+            TreeNode node = stack.poll();
+
+            if (node.left != null) {
+                parent.put(node.left, node);
+                stack.offer(node.left);
+            }
+            if (node.right != null) {
+                parent.put(node.right, node);
+                stack.offer(node.right);
+            }
+        }
+
+        Set<TreeNode> ancestors = new HashSet<>(); //存祖先节点
+
+        while (p != null) { //先把p祖先节点都加入
+            ancestors.add(p);
+            p = parent.get(p);
+        }
+
+        while (!ancestors.contains(q)) //q再往上找，直到出现的第一个即为LCA
+            q = parent.get(q);
+        return q;
     }
 
     public class TreeNode {
