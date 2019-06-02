808ed14c7f20eb6adf6173b7f898a4c98554fd72
rparser <runningparser@gmail.com>
2019-06-01 20:03:24 -0500

    Update _079_WordSearch.java

--- src/com/leetcode/solution/_079_WordSearch.java
+++ src/com/leetcode/solution/_079_WordSearch.java
@@ -7,30 +7,38 @@ package com.leetcode.solution;
  * <p>
  * Time complexity :O(m*n*4^l). l为单词长度
  * Space complexity :O(m*n+l).
+ * <p>
+ * 思路： dfs each character in matrix to see if it can generate a path that match string.
+ * Make the visited node along the path invalid char by changing to '*', so we can avoid repeated search. Once return change back the letter to original.
+ * (Or extra space -- Use a visited[][] matrix to mark each visited node along the path)
+ * <p>
+ * Complexity: time O(mn*4^k) where k is the length of the string; mn for for loop and for the dfs method its 4^k.Since the dfs method goes only as deep as the word length we have T(k)=4(T(k-1))=4*4T(k-2)=....=.. which will be 4^k.
+ * space O(4mn) if the function call stack is taken into account. In each cell, we recursively call its 4four neighbors and there are mn cells in total.
+ * 模板dfs,主函数遍历，首字母和ij等则&&dfs,boolean(r,c,index)函数，index到长度返T,点不同返F，取点修改为*记录已访问，四方向递归or(相等一个就行)，还原回*字母
  */
 
 public class _079_WordSearch {
-    public boolean exist(char[][] board, String word) {
-        if (board == null) return false;
-        for (int row = 0; row < board.length; row++) {
-            for (int col = 0; col < board[0].length; col++)
-                if (existHelper(board, word.toCharArray(), 0, col, row)) return true;
-        }
-        return false;
+    private boolean dfsSearch(char[][] board, String word, int r, int c, int index) {
+        if (index == word.length()) return true; //达到长度
+        if (r < 0 || r >= board.length || c < 0 || c >= board[0].length || word.charAt(index) != board[r][c]) //或不符合
+            return false;
+        board[r][c] = '*'; //or we can do board[i][j]^=255, to make it not a valid letter，代表已访问
+        boolean res = dfsSearch(board, word, r - 1, c, index + 1)
+                || dfsSearch(board, word, r + 1, c, index + 1)
+                || dfsSearch(board, word, r, c - 1, index + 1)
+                || dfsSearch(board, word, r, c + 1, index + 1);
+        board[r][c] = word.charAt(index); //还原字符，recover by doing board[i][j]^=255
+        return res;
     }
 
-    private boolean existHelper(char[][] board, char[] word, int index, int col, int row) {
-        if (index == word.length) return true; //长度已经达到,index为已经递归n次
-        if (row < 0 || col < 0 || row >= board.length || col >= board[0].length) return false; //脱离版面返回false
-        if (board[row][col] != word[index]) return false; //如果字符不匹配或已使用
-        char used = board[row][col]; // used保存当前格子值
-        board[row][col] = 0; //当前格子值设为0，保证不能走
-        boolean exist = existHelper(board, word, index + 1, col + 1, row) //四个方向有一个返回true即可
-                || existHelper(board, word, index + 1, col - 1, row)
-                || existHelper(board, word, index + 1, col, row + 1)
-                || existHelper(board, word, index + 1, col, row - 1);
-        if (exist) return true;
-        board[row][col] = used; //如果没找到，放回格子的值
+    public boolean exist(char[][] board, String word) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                //fist half to save some search; index is for word
+                if ((word.charAt(0) == board[i][j]) && dfsSearch(board, word, i, j, 0))
+                    return true;
+            }
+        }
         return false;
     }
 }
