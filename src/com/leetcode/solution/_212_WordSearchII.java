274e3f5f2935d18d16613810b1efdc47e9ed35cc
rparser <runningparser@gmail.com>
2019-06-01 20:59:27 -0500

    Update _212_WordSearchII.java

--- src/com/leetcode/solution/_212_WordSearchII.java
+++ src/com/leetcode/solution/_212_WordSearchII.java
@@ -4,38 +4,38 @@ import java.util.*;
 
 /**
  * 查找单词2,参考079图中找单词
+ * 模板dfs,主函数遍历，首字母和ij等则&&dfs,boolean(r,c,index)函数，index到长度返T,点不同返F，取点修改为*记录已访问，四方向递归or(相等一个就行)，还原回*字母
  */
 
 public class _212_WordSearchII {
-    public List<String> findWords(char[][] board, String[] words) {
-        List<String> res = new ArrayList<>();
-        TrieNode root = buildTrie(words);
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[0].length; j++) {
-                dfs(board, i, j, root, res);
-            }
-        }
-        return res;
-    }
+    private void dfs(char[][] board, int r, int c, TrieNode node, List<String> list) {
+        if (r < 0 || c < 0 || r >= board.length || c >= board[0].length) return;
 
-    private void dfs(char[][] board, int i, int j, TrieNode p, List<String> res) {
-        char c = board[i][j];
-        if (c == '#' || p.next[c - 'a'] == null) return;
-        p = p.next[c - 'a'];
-        if (p.word != null) {   // found one
-            res.add(p.word);
-            p.word = null;     // de-duplicate
+        char tmp = board[r][c];
+        int n = tmp - 'a';
+        if (tmp == '*' || node.next[n] == null) return; //已访问或next空则返回
+
+        node = node.next[n];
+        board[r][c] = '*'; //设为已访问
+        if (node.word != null) {
+            list.add(node.word);
+            node.word = null; // One time search.
         }
 
-        board[i][j] = '#';
-        if (i > 0) dfs(board, i - 1, j, p, res);
-        if (j > 0) dfs(board, i, j - 1, p, res);
-        if (i < board.length - 1) dfs(board, i + 1, j, p, res);
-        if (j < board[0].length - 1) dfs(board, i, j + 1, p, res);
-        board[i][j] = c;
+        dfs(board, r + 1, c, node, list);
+        dfs(board, r - 1, c, node, list);
+        dfs(board, r, c + 1, node, list);
+        dfs(board, r, c - 1, node, list);
+
+        board[r][c] = tmp; //还原临时存储
     }
 
-    private TrieNode buildTrie(String[] words) {
+    class TrieNode {
+        TrieNode[] next = new TrieNode[26];
+        String word;
+    }
+
+    private TrieNode buildTrie(String[] words) { //构造Trie
         TrieNode root = new TrieNode();
         for (String w : words) {
             TrieNode p = root;
@@ -49,8 +49,14 @@ public class _212_WordSearchII {
         return root;
     }
 
-    class TrieNode {
-        TrieNode[] next = new TrieNode[26];
-        String word;
+    public List<String> findWords(char[][] board, String[] words) {
+        List<String> result = new ArrayList<>();
+        TrieNode root = buildTrie(words);
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                dfs(board, i, j, root, result);
+            }
+        }
+        return result;
     }
 }
