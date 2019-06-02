0a4fd93e4cf9984e98a14d788800b152d08a6061
rparser <runningparser@gmail.com>
2019-06-01 19:02:24 -0500

    Update _694_NumberofDistinctIslands.java

--- src/com/leetcode/solution/_694_NumberofDistinctIslands.java
+++ src/com/leetcode/solution/_694_NumberofDistinctIslands.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 /**
  * 岛屿形状统计
- * 相对于200，增加set记录岛屿形状
+ * 相对于200，增加2set记录1岛屿形状2把1toString加入2，统计2大小
  */
 
 public class _694_NumberofDistinctIslands {
@@ -14,7 +14,7 @@ public class _694_NumberofDistinctIslands {
         }
         set.add((r - baseX) + "_" + (c - baseY)); //比200多的：encode变为字符串,记录相对岛屿形状
         grid[r][c] = 0;
-        dfs(grid, r + 1, c, baseX, baseY, set);
+        dfs(grid, r + 1, c, baseX, baseY, set); //每个顶点相对位置
         dfs(grid, r - 1, c, baseX, baseY, set);
         dfs(grid, r, c - 1, baseX, baseY, set);
         dfs(grid, r, c + 1, baseX, baseY, set);
