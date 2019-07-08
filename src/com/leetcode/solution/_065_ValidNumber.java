53e17637353886fd55155f443d46d4e39ed593e2
rparser <runningparser@gmail.com>
2019-07-08 05:10:16 -0500

    Update _065_ValidNumber.java

--- src/com/leetcode/solution/_065_ValidNumber.java
+++ src/com/leetcode/solution/_065_ValidNumber.java
@@ -26,4 +26,24 @@ public class _065_ValidNumber {
         }
         return numberSeen;
     }
+
+    public boolean isNumberwithoutE(String s) {
+        s = s.trim();
+        boolean hasPoint = false;
+        boolean isNumber = false;
+        for (int i = 0; i < s.length(); i++) {
+            if ('0' <= s.charAt(i) && s.charAt(i) <= '9') {
+                isNumber = true;
+            } else if (s.charAt(i) == '.') {
+                if (hasPoint)
+                    return false;
+                hasPoint = true; // first char can be '.'
+            } else if (s.charAt(i) == '-' || s.charAt(i) == '+') {
+                if (i != 0)
+                    return false;
+            } else
+                return false;
+        }
+        return isNumber;
+    }
 }
