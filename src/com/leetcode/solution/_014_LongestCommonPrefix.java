24e65207d33e1e8a517a0bda442d5474b8123073
rparser <runningparser@gmail.com>
2019-07-16 04:53:39 -0500

    Update _014_LongestCommonPrefix.java

--- src/com/leetcode/solution/_014_LongestCommonPrefix.java
+++ src/com/leetcode/solution/_014_LongestCommonPrefix.java
@@ -1,4 +1,21 @@
 package com.leetcode.solution;
 
+/**
+ * Time complexity : O(S) , where S is the sum of all characters in all strings.
+ * Space complexity : O(1). We only used constant extra space.
+ * Time complexity: (mk + k^2)
+ */
+
 public class _014_LongestCommonPrefix {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0) return "";
+        String ans = "";
+        for (int i = 0; i < strs[0].length(); ++i) {
+            char c = strs[0].charAt(i);
+            for (String s : strs)
+                if (s.length() <= i || s.charAt(i) != c) return ans;
+            ans += c;
+        }
+        return ans;
+    }
 }
