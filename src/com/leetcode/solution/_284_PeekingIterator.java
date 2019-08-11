0a73c79001bbfe2efdafe727f9e4df0f5bc234de
rparser <runningparser@gmail.com>
2019-08-11 04:21:47 -0500

    Create _284_PeekingIterator.java

---
+++ src/com/leetcode/solution/_284_PeekingIterator.java
@@ -0,0 +1,35 @@
+package com.leetcode.solution;
+
+import java.util.*;
+
+public class _284_PeekingIterator implements Iterator<Integer> {
+    Iterator<Integer> it;
+    Integer next = null;
+
+    public _284_PeekingIterator(Iterator<Integer> iterator) {
+        // initialize any member here.
+        it = iterator;
+        if (it.hasNext()) {
+            next = it.next();
+        }
+    }
+
+    // Returns the next element in the iteration without advancing the iterator.
+    public Integer peek() {
+        return next;
+    }
+
+    // hasNext() and next() should behave the same as in the Iterator interface.
+    // Override them if needed.
+    @Override
+    public Integer next() {
+        Integer res = next;
+        next = it.hasNext() ? it.next() : null;
+        return res;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return next != null;
+    }
+}
