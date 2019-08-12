ad1bdf7c5654c392ca93c3c428cf54956d884d7c
rparser <runningparser@gmail.com>
2019-08-12 03:42:28 -0500

    Update _873_LengthofLongestFibonacciSubsequence.java

--- src/com/leetcode/solution/_873_LengthofLongestFibonacciSubsequence.java
+++ src/com/leetcode/solution/_873_LengthofLongestFibonacciSubsequence.java
@@ -1,5 +1,20 @@
 package com.leetcode.solution;
 
-public class _873_LengthofLongestFibonacciSubsequence {
+import java.util.*;
 
+public class _873_LengthofLongestFibonacciSubsequence {
+    public int lenLongestFibSubseq(int[] A) {
+        int res = 0;
+        int[][] dp = new int[A.length][A.length];
+        Map<Integer, Integer> index = new HashMap<>();
+        for (int j = 0; j < A.length; j++) {
+            index.put(A[j], j);
+            for (int i = 0; i < j; i++) {
+                int k = index.getOrDefault(A[j] - A[i], -1);
+                dp[i][j] = (A[j] - A[i] < A[i] && k >= 0) ? dp[k][i] + 1 : 2;
+                res = Math.max(res, dp[i][j]);
+            }
+        }
+        return res > 2 ? res : 0;
+    }
 }
