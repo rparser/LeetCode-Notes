b176216dbc52047cb3ca4be4f95f3085cc6ebaa3
rparser <runningparser@gmail.com>
2019-08-14 20:30:04 -0500

    Update UnionFindSet.java

--- src/com/leetcode/common/UnionFindSet.java
+++ src/com/leetcode/common/UnionFindSet.java
@@ -1,15 +1,15 @@
 package com.leetcode.common;
 
 class UnionFindSet {
-    private int[] parents_;
-    private int[] ranks_;
+    private int[] parents;
+    private int[] ranks;
 
     public UnionFindSet(int n) {
-        parents_ = new int[n + 1];
-        ranks_ = new int[n + 1];
-        for (int i = 0; i < parents_.length; ++i) {
-            parents_[i] = i;
-            ranks_[i] = 1;
+        parents = new int[n + 1];
+        ranks = new int[n + 1];
+        for (int i = 0; i < parents.length; ++i) {
+            parents[i] = i;
+            ranks[i] = 1;
         }
     }
 
@@ -18,20 +18,19 @@ class UnionFindSet {
         int pv = Find(v);
         if (pu == pv) return false;
 
-        if (ranks_[pv] > ranks_[pu]) parents_[pu] = pv;
-        else if (ranks_[pu] > ranks_[pv]) parents_[pv] = pu;
+        if (ranks[pv] > ranks[pu]) parents[pu] = pv;
+        else if (ranks[pu] > ranks[pv]) parents[pv] = pu;
         else {
-            parents_[pv] = pu;
-            ranks_[pu] += 1;
+            parents[pv] = pu;
+            ranks[pu] += 1;
         }
-
         return true;
     }
 
     public int Find(int u) {
-        while (parents_[u] != u) {
-            parents_[u] = parents_[parents_[u]];
-            u = parents_[u];
+        while (parents[u] != u) {
+            parents[u] = parents[parents[u]];
+            u = parents[u];
         }
         return u;
     }
