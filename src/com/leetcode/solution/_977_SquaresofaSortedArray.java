6fccc0a711880a8857f0d436b16637532ca02bb2
rparser <runningparser@gmail.com>
2019-08-15 21:45:51 -0500

    977

---
+++ src/com/leetcode/solution/_977_SquaresofaSortedArray.java
@@ -0,0 +1,19 @@
+package com.leetcode.solution;
+
+public class _977_SquaresofaSortedArray {
+    public int[] sortedSquares(int[] A) {
+        int n = A.length;
+        int[] result = new int[n];
+        int i = 0, j = n - 1;
+        for (int p = n - 1; p >= 0; p--) {
+            if (Math.abs(A[i]) > Math.abs(A[j])) {//比较两个绝对值 绝对值大则放在后面
+                result[p] = A[i] * A[i];
+                i++;
+            } else {
+                result[p] = A[j] * A[j];
+                j--;
+            }
+        }
+        return result;
+    }
+}
