874a6d4b51d457d2d9f3fece312b97b58ed2cb6c
rparser <runningparser@gmail.com>
2019-09-22 17:55:47 -0500

    Update _003_LongestSubstringWithoutRepeatingCharacters.java

--- src/com/leetcode/solution/_003_LongestSubstringWithoutRepeatingCharacters.java
+++ src/com/leetcode/solution/_003_LongestSubstringWithoutRepeatingCharacters.java
@@ -11,12 +11,12 @@ import java.util.*;
  * The size of the Set is upper bounded by the size of the string n and the size of the charset/alphabet m.
  */
 
-public class _003LongestSubstringWithoutRepeatingCharacters {
+public class _003_LongestSubstringWithoutRepeatingCharacters {
     public int lengthOfLongestSubstring(String s) {
         int n = s.length(), result = 0;
         Map<Character, Integer> map = new HashMap<>(); // current index of character
         for (int j = 0, i = 0; j < n; j++) {
-            // 如果新加入的j字母已经有了, i有可能要前移（移动到）
+            // 如果新加入的j字母已经有了, i有可能要前移（移动到当前值和存储值的max,必须是max因为i不能后退（如果只是存储值可能会后退））
             if (map.containsKey(s.charAt(j))) i = Math.max(map.get(s.charAt(j)), i);
             result = Math.max(result, j - i + 1); // 更新result
             map.put(s.charAt(j), j + 1); // 加入当前字母对应的最后位置+1
