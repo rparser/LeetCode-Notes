bf745e02cae7eacf87450fd36e4fdca0a6bbf91d
rparser <runningparser@gmail.com>
2019-09-23 12:58:34 -0500

    Update _239_SlidingWindowMaximum.java

--- src/com/leetcode/solution/_239_SlidingWindowMaximum.java
+++ src/com/leetcode/solution/_239_SlidingWindowMaximum.java
@@ -1,30 +1,24 @@
 package com.leetcode.solution;
 
+import java.util.*;
+
+/**
+ * amortized O(n)
+ */
+
 public class _239_SlidingWindowMaximum {
     public int[] maxSlidingWindow(int[] nums, int k) {
         int n = nums.length;
-        if (n * k == 0) return new int[0];
-        if (k == 1) return nums;
-
-        int[] left = new int[n];
-        left[0] = nums[0];
-        int[] right = new int[n];
-        right[n - 1] = nums[n - 1];
-        for (int i = 1; i < n; i++) {
-            // from left to right
-            if (i % k == 0) left[i] = nums[i];  // block_start
-            else left[i] = Math.max(left[i - 1], nums[i]);
-
-            // from right to left
-            int j = n - i - 1;
-            if ((j + 1) % k == 0) right[j] = nums[j];  // block_end
-            else right[j] = Math.max(right[j + 1], nums[j]);
-        }
+        if (n == 0) return nums;
 
         int[] result = new int[n - k + 1];
-        for (int i = 0; i < n - k + 1; i++)
-            result[i] = Math.max(left[i + k - 1], right[i]);
-
+        Deque<Integer> dq = new ArrayDeque<>();
+        for (int i = 0; i < n; i++) {
+            if (!dq.isEmpty() && dq.peek() < i - k + 1) dq.poll(); // 移掉超出范围的
+            while (!dq.isEmpty() && nums[i] >= nums[dq.peekLast()]) dq.pollLast(); // 如果k范围内的，新来的值比前值更小，移去前值
+            dq.offer(i); // dq contains index... nums contains content
+            if (i - k + 1 >= 0) result[i - k + 1] = nums[dq.peek()]; //更新结果
+        }
         return result;
     }
 }
