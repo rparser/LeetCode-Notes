726da831860dda76b63f588f89dcb668af821e16
rparser <runningparser@gmail.com>
2019-09-23 02:03:28 -0500

    Update _759_EmployeeFreeTime.java

--- src/com/leetcode/solution/_759_EmployeeFreeTime.java
+++ src/com/leetcode/solution/_759_EmployeeFreeTime.java
@@ -35,19 +35,17 @@ public class _759_EmployeeFreeTime {
     }
 
     public List<Interval> employeeFreeTime(List<List<Interval>> avails) {
-
         List<Interval> result = new ArrayList<>();
-
         PriorityQueue<Interval> pq = new PriorityQueue<>((a, b) -> a.start - b.start);
         avails.forEach(e -> pq.addAll(e));
 
         Interval temp = pq.poll();
         while (!pq.isEmpty()) {
             if (temp.end < pq.peek().start) { // no intersect
-                result.add(new Interval(temp.end, pq.peek().start));
+                result.add(new Interval(temp.end, pq.peek().start)); //无交集则把中间加入
                 temp = pq.poll(); // becomes the next temp interval
             } else { // intersect or sub merged
-                temp = temp.end < pq.peek().end ? pq.peek() : temp;
+                temp = temp.end < pq.peek().end ? pq.peek() : temp; //有交集则合并
                 pq.poll();
             }
         }
