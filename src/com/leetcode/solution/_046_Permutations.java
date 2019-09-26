3f2e1387116cd4a0768edcae77288d5d9050c156
rparser <runningparser@gmail.com>
2019-09-26 01:05:42 -0500

    031 046

--- src/com/leetcode/solution/_046_Permutations.java
+++ src/com/leetcode/solution/_046_Permutations.java
@@ -31,4 +31,25 @@ public class _046_Permutations {
             used[i] = false;
         }
     }
+
+    public void backtrack(int n, ArrayList<Integer> nums, List<List<Integer>> output, int first) {
+        if (first == n)  // if all integers are used up
+            output.add(new ArrayList<Integer>(nums));
+        for (int i = first; i < n; i++) {
+            Collections.swap(nums, first, i); // place i-th integer first in the current permutation
+            backtrack(n, nums, output, first + 1); // use next integers to complete the permutations
+            Collections.swap(nums, first, i); // backtrack
+        }
+    }
+
+    public List<List<Integer>> permutebacktrack(int[] nums) {
+        List<List<Integer>> result = new LinkedList<>();
+        // convert nums into list since the output is a list of lists
+        ArrayList<Integer> nums_lst = new ArrayList<>();
+        for (int num : nums) nums_lst.add(num);
+
+        int n = nums.length;
+        backtrack(n, nums_lst, result, 0);
+        return result;
+    }
 }
