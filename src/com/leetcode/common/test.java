c6632546a24652375a18f54f0ddeefda5ab90729
rparser <runningparser@gmail.com>
2019-09-27 18:16:54 -0500

    Update test.java

--- src/com/leetcode/common/test.java
+++ src/com/leetcode/common/test.java
@@ -1,4 +1,20 @@
 package com.leetcode.common;
 
+import org.junit.runner.JUnitCore;
+
 public class test {
+    public static int holes(int num) {
+        String str = Integer.toString(num);
+        int count = 0;
+        for (char c : str.toCharArray()) {
+            if (c == '8') count += 2;
+            else if (c == '0' || c == '4' || c == '6' || c == '9') count++;
+        }
+        return count;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(holes(1288));
+    }
 }
