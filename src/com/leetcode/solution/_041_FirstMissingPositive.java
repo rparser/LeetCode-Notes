53958842bbd5300600595f98abd1d9567f91ddf2
rparser <runningparser@gmail.com>
2019-09-29 04:07:01 -0500

    Update _041_FirstMissingPositive.java

--- src/com/leetcode/solution/_041_FirstMissingPositive.java
+++ src/com/leetcode/solution/_041_FirstMissingPositive.java
@@ -2,20 +2,28 @@ package com.leetcode.solution;
 
 public class _041_FirstMissingPositive {
     public int firstMissingPositive(int[] nums) {
-        for (int i = 0; i < nums.length; i++)
-            while (nums[i] > 0 && nums[i] <= nums.length && nums[nums[i] - 1] != nums[i])
-                swap(nums, i, nums[i] - 1);
+        int n = nums.length;
 
-        for (int i = 0; i < nums.length; i++)
-            if (nums[i] != i + 1)
-                return i + 1;
+        for (int i = 0; i < n; i++)
+            if (nums[i] <= 0 || nums[i] > n)
+                nums[i] = n + 1; //把负数和过大的数（无关的）变成n+1，所以所有数会在1...n+1之间
 
-        return nums.length + 1;
-    }
+        // 2. mark each cell appearing in the array, by converting the index for that number to negative
+        for (int i = 0; i < n; i++) {
+            int cur = Math.abs(nums[i]); //找到绝对值
+            System.out.println(nums[i]+"ccc");
+            if (cur > n) continue; //超出范围则跳过
 
-    private void swap(int[] nums, int i, int j) {
-        int tmp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = tmp;
+            cur--; // -1 for zero index based array (so the number 1 will be at pos 0)
+            if (nums[cur] > 0) nums[cur] = -1 * nums[cur]; //范围内的正数变负数
+            System.out.println("nums[cur]"+nums[cur]);
+        }
+
+        // 3. find the first cell which isn't negative (doesn't appear in the array)
+        for (int i = 0; i < n; i++)
+            if (nums[i] >= 0) return i + 1;
+
+        // 4. no positive numbers were found, which means the array contains all numbers 1..n
+        return n + 1;
     }
 }
