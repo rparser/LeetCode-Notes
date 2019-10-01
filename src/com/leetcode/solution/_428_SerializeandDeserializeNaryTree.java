06af33ff2f96be5f69e64c1c40e5380d3166d88f
rparser <runningparser@gmail.com>
2019-10-01 01:10:46 -0500

    Update _428_SerializeandDeserializeNaryTree.java

--- src/com/leetcode/solution/_428_SerializeandDeserializeNaryTree.java
+++ src/com/leetcode/solution/_428_SerializeandDeserializeNaryTree.java
@@ -2,18 +2,6 @@ package com.leetcode.solution;
 
 import java.util.*;
 
-class Node {
-    public int val;
-    public List<Node> children;
-
-    public Node() {
-    }
-
-    public Node(int _val, List<Node> _children) {
-        val = _val;
-        children = _children;
-    }
-};
 
 public class _428_SerializeandDeserializeNaryTree {
     // Encodes a tree to a single string.
@@ -48,5 +36,18 @@ public class _428_SerializeandDeserializeNaryTree {
         root.children = new ArrayList<Node>(size);
         for (int i = 0; i < size; i++) root.children.add(deserializeHelper(q));
         return root;
+    }
+
+    class Node {
+        public int val;
+        public List<Node> children;
+
+        public Node() {
+        }
+
+        public Node(int _val, List<Node> _children) {
+            val = _val;
+            children = _children;
+        }
     }
 }
