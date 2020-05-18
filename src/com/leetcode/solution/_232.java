1de2dd45a067c2cd56f1a015b13ab6b224afc037
Tony Zhang <tozhang@groupon.com>
2020-05-18 14:53:07 -0500

    update

---
+++ src/com/leetcode/solution/_232.java
@@ -0,0 +1,45 @@
+public class MyQueue232 {
+    private Stack<Integer> in = null;
+    private Stack<Integer> out = null;
+    /**
+     * Initialize your data structure here.
+     */
+    public MyQueue232() {
+        in = new Stack<>();
+        out = new Stack<>();
+    }
+    /**
+     * Push element x to the back of queue.
+     */
+    public void push(int x) {
+        in.push(x);
+    }
+    /**
+     * Removes the element from in front of queue and returns that element.
+     */
+    public int pop() {
+        if (out.empty()) {
+            int size = in.size();
+            for (int i = 0; i < size; i++)
+                out.push(in.pop());
+        }
+        return out.pop();
+    }
+    /**
+     * Get the front element.
+     */
+    public int peek() {
+        if (out.empty()) {
+            int size = in.size();
+            for (int i = 0; i < size; i++)
+                out.push(in.pop());
+        }
+        return out.peek();
+    }
+    /**
+     * Returns whether the queue is empty.
+     */
+    public boolean empty() {
+        return in.empty() && out.empty();
+    }
+}
