c981e557ed2e6d35e91741be88d07da31b3b0732
rparser <runningparser@gmail.com>
2020-06-28 03:32:42 -0700

    update

--- src/com/leetcode/solution/_340_longest-substring-with-at-most-k-distinct-characters.java
+++ src/com/leetcode/solution/_340_longest-substring-with-at-most-k-distinct-characters.java
@@ -9,13 +9,51 @@ import java.util.*;
  */
 
 public class _340_LongestSubstringwithAtMostKDistinctCharacters {
+    // O(N)解法
+    /**
+     * 为了达到 O(N) 的效率，我们可以使用LinkedHashMap，保证以下四种操作都可以在 O(1) 时间完成：HashMap不能保证返回最先插入对
+     * 插入键
+     * 获取键 / 检查键是否存在
+     * 删除键
+     * 返回最先 / 最后插入的键值对
+     **/
     public int lengthOfLongestSubstringKDistinct(String s, int k) {
+        int n = s.length();
+        if (n * k == 0) return 0;
+
+        int left = 0;
+        int right = 0;
+        // 字符和它最右的index
+        LinkedHashMap<Character, Integer> map = new LinkedHashMap<>(k + 1);
+        int max_len = 1;
+
+        while (right < n) {
+            Character c = s.charAt(right);
+            // 必须先删除，这样c就是最后加入的了（否则c可能在中间更新）
+            map.remove(c);
+            map.put(c, right);
+            right++;
+
+            // slidewindow contains k + 1 characters
+            if (map.size() > k) {
+                // 找到map里第一个entry
+                Map.Entry<Character, Integer> leftmost = map.entrySet().iterator().next();
+                map.remove(map.entrySet().iterator().next().getKey());
+                // 删除了之前最左的，所以此时窗口左指针+1
+                left = leftmost.getValue() + 1;
+            }
+            max_len = Math.max(max_len, right - left);
+        }
+        return max_len;
+    }
+
+    //O(NK)解法
+    public int lengthOfLongestSubstringKDistinctHashMap(String s, int k) {
         int n = s.length();
         if (n * k == 0) return 0;
         int left = 0, right = 0; // sliding window left and right pointers
         int max_len = 1;
-        //hashmap character -> its rightmost position in the sliding window
-        HashMap<Character, Integer> map = new HashMap<>();
+        HashMap<Character, Integer> map = new HashMap<>(); //hashmap character -> its rightmost position in the sliding window
 
         while (right < n) {
             map.put(s.charAt(right), right++); // add new character and move right pointer
